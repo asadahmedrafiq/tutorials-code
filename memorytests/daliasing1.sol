3d5317182f788d4ecfec270a1e8a10771a883bc8
Asad Ahmed <asadahmedrafiq@gmail.com>
2025-07-16 13:27:58 +0100

    Added daliasing1.sol

---
+++ memorytests/daliasing1.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+// Memory test for pointer-to-pointer copyingbehavior (/certora/tutorials-code/memorytests/aliasing1.sol)
+
+pragma solidity ^0.8.0;
+
+
+/**
+ * @dev Implementation of assignment behavior in multi-dimenstional memory arrays
+ *
+ * This contract accepts two static two-dimensional memory arrays and indeces to perform assignment operation.
+ * Furhter one of the array data/content is loaded. This then serves us to obse3rve the behavior of the memory assignment of such arrays.
+ * The function does assignment operation and returns one of the memory array for the purpose of validating the assignment behavior.
+ *
+ */
+contract aliasing1 {
+   
+    constructor() {
+    }
+
+ function assign3(uint8[5][5] memory x, uint8 i, uint8 j, uint8 k, uint8 value, uint8[5][5] memory y) 
+    public 
+    returns (uint8[5][5] memory)  
+    {
+      x[i] = y[j];
+      y[j][k] = value;
+      return x;
+    }
+
+   }
